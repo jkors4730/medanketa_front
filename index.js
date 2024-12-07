0b63412184c974f86e23898bcfb5f515d9b31130
jkors4730 <larnikko1@yandex.ru>
2024-12-08 00:16:26 +0300

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(`${process.env.FRONT}/dist`));
+
+app.all('*',(_req, res) => {
+    res.sendFile( path.join(__dirname, `.${process.env.FRONT}/dist/index.html`) );
+});
+
+app.listen(process.env.PORT);
